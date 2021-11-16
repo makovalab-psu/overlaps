612a3420c664d10895cc4769b5fb7390b873f987
Nick <nmapsy@gmail.com>
2021-11-15 20:26:23 -0500

    read_errors.py: Rewrite to make Errors attributes of Pairs and Overlaps their own tuple.

--- read_errors.py
+++ read_errors.py
@@ -22,39 +22,97 @@ def bool_literal(raw_value):
     raise ValueError(f'Invalid bool literal {raw_value}')
 
 
-ERROR_METADATA = [
-  {'key':'type', 'type':str},
-  {'key':'chr', 'type':str},
-  {'key':'pos', 'type':int},
-  {'key':'read_coord1', 'type':int},
-  {'key':'read_coord2', 'type':int},
-  {'key':'base1', 'type':str},
-  {'key':'base2', 'type':str},
-  {'key':'qual1', 'type':str},
-  {'key':'qual2', 'type':str},
+PAIR_METADATA = [                         # Column
+  None,                                   # 1
+  {'key':'name', 'type':str},             # 2
+  {'key':'mapped', 'type':bool_literal},  # 3
+  {'key':'rlen1', 'type':int},            # 4
+  {'key':'rlen2', 'type':int},            # 5
+  None,                                   # 6
+  None,                                   # 7
+  {'key':'forward', 'type':bool_literal}, # 8
+  None,                                   # 9
+  None,                                   # 10
 ]
-PAIR_METADATA = [
-  {'key':'name', 'type':str},
-  {'key':'mapped', 'type':bool_literal},
-  {'key':'rlen1', 'type':int},
-  {'key':'rlen2', 'type':int},
-  {'key':'overlap', 'type':int},
-  {'key':'errors', 'type':int},
-  {'key':'forward', 'type':bool_literal},
-  {'key':'ostart', 'type':int},
-  {'key':'oend', 'type':int},
+OVERLAP_METADATA = [           # Column
+  None,                        # 1
+  None,                        # 2
+  None,                        # 3
+  None,                        # 4
+  None,                        # 5
+  {'key':'len', 'type':int},   # 6
+  None,                        # 7
+  None,                        # 8
+  {'key':'start', 'type':int}, # 9
+  {'key':'end', 'type':int},   # 10
+]
+ERROR_METADATA = [                   # Column
+  None,                              # 1
+  {'key':'type', 'type':str},        # 2
+  {'key':'chr', 'type':str},         # 3
+  {'key':'pos', 'type':int},         # 4
+  {'key':'read_coord1', 'type':int}, # 5
+  {'key':'read_coord2', 'type':int}, # 6
+  {'key':'base1', 'type':str},       # 7
+  {'key':'base2', 'type':str},       # 8
+  {'key':'qual1', 'type':str},       # 9
+  {'key':'qual2', 'type':str},       # 10
 ]
 
+def get_fields(metadata):
+  fields = []
+  for meta in metadata:
+    if meta is not None:
+      fields.append(meta['key'])
+  return fields
 
-Error = collections.namedtuple('Error', [meta['key'] for meta in ERROR_METADATA]+['pair'])
-Pair = collections.namedtuple('Pair', [meta['key'] for meta in PAIR_METADATA])
-Error.__eq__ = Pair.__eq__ = tuple_eq
+Pair = collections.namedtuple('Pair', get_fields(PAIR_METADATA)+['overlap','errors'])
+Overlap = collections.namedtuple('Overlap', get_fields(OVERLAP_METADATA))
+Error = collections.namedtuple('Error', get_fields(ERROR_METADATA))
+Error.__eq__ = Pair.__eq__ = Overlap.__eq__ = tuple_eq
+
+
+def read_errors_file(errors_file):
+  """Parse the errors file and return the data.
+  This will yield each Pair in the errors file. This includes the data in the "error" lines, since
+  each Pair includes every associated Error as a list in its `errors` field."""
+  pair = None
+  for line_num, line_raw in enumerate(errors_file,1):
+    value_strs = line_raw.rstrip('\r\n').split('\t')
+    if value_strs[0] == 'pair':
+      if pair is not None:
+        yield pair
+      if len(value_strs) != len(PAIR_METADATA):
+        raise ValueError(
+          f'Error on line {line_num}: Wrong number of columns (expected {len(PAIR_METADATA)}, '
+          f'saw {len(value_strs)})'
+        )
+      overlap_values = parse_fields(value_strs, OVERLAP_METADATA)
+      overlap = Overlap(**overlap_values)
+      pair_values = parse_fields(value_strs, PAIR_METADATA)
+      pair = Pair(overlap=overlap, errors=[], **pair_values)
+    elif value_strs[0] == 'error':
+      if len(value_strs) != len(ERROR_METADATA):
+        raise ValueError(
+          f'Error on line {line_num}: Wrong number of columns (expected {len(ERROR_METADATA)}, '
+          f'saw {len(value_strs)})'
+        )
+      error_values = parse_fields(value_strs, ERROR_METADATA)
+      error = Error(**error_values)
+      pair.errors.append(error)
+  if pair is not None:
+    yield pair
 
 
 def read_errors(errors_file):
+  """Only read the errors from the errors file.
+  For performance, this skips parsing "pair" lines with no associated errors.
+  This still yields a series of Pairs, but only the Pairs with at least one Error."""
   pair_line = pair = None
   for line_raw in errors_file:
     if line_raw.startswith('pair\t'):
+      if pair is not None:
+        yield pair
       # Don't immediately parse this and create a new Pair.
       # There are often way more pair lines than errors, so that would usually be a waste of time.
       pair_line = line_raw
@@ -63,23 +121,29 @@ def read_errors(errors_file):
       pair = None
     elif line_raw.startswith('error\t'):
       if pair is None:
-        pair_fields = pair_line.rstrip('\r\n').split('\t')
-        pair = parse_fields(pair_fields[1:], Pair, PAIR_METADATA)
-      error_fields = line_raw.rstrip('\r\n').split('\t')
-      error = parse_fields(error_fields[1:], Error, ERROR_METADATA, pair=pair)
-      yield error
+        value_strs = pair_line.rstrip('\r\n').split('\t')
+        overlap_values = parse_fields(value_strs, OVERLAP_METADATA)
+        overlap = Overlap(**overlap_values)
+        pair_values = parse_fields(value_strs, PAIR_METADATA)
+        pair = Pair(overlap=overlap, errors=[], **pair_values)
+      value_strs = line_raw.rstrip('\r\n').split('\t')
+      error_values = parse_fields(value_strs, ERROR_METADATA)
+      error = Error(**error_values)
+      pair.errors.append(error)
     else:
       raise ValueError(f'Invalid first field in line {line_raw!r}')
+  if pair is not None:
+    yield pair
 
 
-def parse_fields(raw_fields, tuple_class, metadata, null='.', **kwargs):
+def parse_fields(raw_fields, metadata, null='.'):
   data = {}
   for raw_value, meta in zip(raw_fields, metadata):
+    if meta is None:
+      continue
     key = meta['key']
     if raw_value == null:
       data[key] = None
     else:
       data[key] = meta['type'](raw_value)
-  for key, value in kwargs.items():
-    data[key] = value
-  return tuple_class(**data)
+  return data
